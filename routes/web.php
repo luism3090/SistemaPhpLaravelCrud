8ff13750665eafc7815d204e4dd4797439235d88
= <=>
2025-04-20 11:39:54 -0600

    Segundo commit

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmpleadoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 
 Route::resource('empleado',EmpleadoController::class);
+Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [EmpleadoController::class, 'index'])->name('home');
+
+Route::group(['middleware'=>'auth'], function(){
+
+    Route::get('/home', [EmpleadoController::class, 'index'])->name('home');
+
+});
+
+
